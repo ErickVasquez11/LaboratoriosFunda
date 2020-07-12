1ddb406bbf0ee3d2e3c85d2421c8aa2579668246
ErickVasquez11 <00065520@uca.edu.sv>
2020-07-11 19:59:47 -0600

    master

--- ExamenFinal/ExamenFinal.cpp
+++ ExamenFinal/ExamenFinal.cpp
@@ -5,47 +5,48 @@ using namespace std;
 const int longCad = 20;   // Longitud de la cadena
 struct CostoPorArticulo{         // declaracion de variable del tipo de la estructura como un nuevo tipo de dato.
     char NombreArticulo[longCad + 1]; // declaracion de una cadena de Longitud carcacteres
-    int Cantidad;
+    int CantidadProducto;
     float Precio;
     float CostoPorArticulo;
 };
 
-void DatosNombre(CostoPorArticulo Articulo[], int Cantidad){
+void DatosNombre(CostoPorArticulo Articulo[], int CantidadProducto){
 
         cout << '\n' << endl;
         cout << " Ingrese los datos de los articulos  " <<endl;
-        for(int i = 0; i < Cantidad; i++){                  
+        for(int i = 0; i < CantidadProducto; i++){                  
             cin.ignore(100,  '\n');        // no lea el enter y sale a la siguiente iteracion.
-            cout << " Ingrese Nombre del Articulo : ";
+            cout << " Nombre del Articulo : ";
             cin.getline(Articulo[i].NombreArticulo, 21 , '\n'); // lee incluye espacios
-            cout << " Ingrese Cantidad de Articulo: ";
-            cin >> Articulo[i].Cantidad; // ordena y guarda en la variable indicada
-            cout << " Ingrese el Precio Unitario : $";
+            cout << " Cantidad de Articulo: ";
+            cin >> Articulo[i].CantidadProducto; // ordena y guarda en la variable indicada
+            cout << " Precio Unitario : $";
             cin >> Articulo[i].Precio;
             cout << "******* Contenido de Compra ****** " << endl; // Titulo de la factura y separador de compra.
         }
 
 }
 
-void CostoArticulo(CostoPorArticulo Articulo[], int Cantidad){
-     for(int i = 0; i < Cantidad; i++){
-        Articulo[i].CostoPorArticulo = Articulo[i].Precio*Articulo[i].Cantidad;  // ordena y guarda la variable es igual precio por el articulo que seria el costo del articulo.
+void CostoArticulo(CostoPorArticulo Articulo[], int CantidadProducto){
+     for(int i = 0; i < CantidadProducto; i++){
+        Articulo[i].CostoPorArticulo = Articulo[i].Precio*Articulo[i].CantidadProducto;  // ordena y guarda la variable es igual precio por el articulo que seria el costo del articulo.
     }
 }
 
-void MostrarDatos(CostoPorArticulo Articulo[], int Cantidad){
+void MostrarDatos(CostoPorArticulo Articulo[], int CantidadProducto){
     
-    for(int i = 0; i < Cantidad; i++){
-        cout << " Nombre del Articulo: " << Articulo[i].NombreArticulo << endl;  // guarda y ordena los datos en la estructura.
-        cout << " Cantidad de Articulos: " << Articulo[i].Cantidad << endl;
+    for(int i = 0; i < CantidadProducto; i++){
+        cout << "* Nombre del Articulo: " << Articulo[i].NombreArticulo << endl;  // guarda y ordena los datos en la estructura.
+        cout << " Cantidad de Articulos: " << Articulo[i].CantidadProducto << endl;
         cout << " Precio Unitario del Articulo: " << Articulo[i].Precio << endl;
-        cout << " Precio Total: $" << Articulo[i].CostoPorArticulo << endl;
+        cout << " Precio Total: $ " << Articulo[i].CostoPorArticulo << endl;
+        cout << "\n";
     }
 }
 
-float CostoTotalCompra(CostoPorArticulo Articulo[],int Cantidad){
+float CostoTotalCompra(CostoPorArticulo Articulo[],int CantidadProducto){
     float Total;
-    for(int i = 0; i < Cantidad; i++){                            // donde el total sera igual al costo por articulo mas total ordenado en la variable.
+    for(int i = 0; i < CantidadProducto; i++){                            // donde el total sera igual al costo por articulo mas total ordenado en la variable.
         Total = Articulo[i].CostoPorArticulo + Total; 
     }
     return Total;
